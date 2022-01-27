32177dcb9d649a4fa57478851f1ec3dee81153f8
Maia Duek <duekmaia@gmail.com>
2022-01-27 16:40:37 -0500

    'earnings' instead of 'score'

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ function animate() {
   ctx.restore();
   ctx.fillStyle = "white";
   ctx.font = '25px sans-serif';
-  ctx.fillText(`SCORE: ${player.score}`, 100, 35)
+  ctx.fillText(`EARNINGS: ${player.score}`, 100, 35)
   ctx.fillText(`LEVEL: ${player.level}`, 250, 35)
 
   for(let i = 0; i < ducksArr.length; i++) {
@@ -246,7 +246,7 @@ function gameOver() {
   ctx.fillText(`GAME OVER`, 375, 100);
   ctx.font = '30px sans-serif';
   ctx.fillStyle = "white";
-  ctx.fillText(`SCORE: ${player.score}`, 405, 150)
+  ctx.fillText(`EARNINGS: ${player.score}`, 405, 150)
   ctx.font = '20px sans-serif';
   ctx.fillText(`Tough luck - you missed too many ducks!`, 310, 200);
   sadTrombone.play();
